a91ba36e28caf7a52989c3d17dee5cadd6505769
mike connor <mike@mikeconnor.tech>
2023-05-05 16:45:46 -0500

    add integration - pnpm astro add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    drafts: true,
+    drafts: true
   },
   integrations: [mdx({
-    drafts: true,
-  })]
+    drafts: true
+  }), react()]
 });
