7bab2018e6fd221e0cc2ba21ea110a9b954088f9
mike connor <mike@mikeconnor.tech>
2023-05-16 12:50:40 -0500

    tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
-import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
 
-import image from "@astrojs/image";
+
 
 // https://astro.build/config
+// your configuration options here...
+  // https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
   markdown: {
     drafts: true
   },
-  integrations: [mdx({
-    drafts: true
-  }), react(), image()]
-});
+  integrations: [
+    mdx({
+      drafts: true
+    }),
+    react()
+  ],
+
+  experimental: {
+    assets: true
+  }
+})
