4ab6fdbce7e0d2b188df6c9f7a24018649b1c39e
mike connor <mike@mikeconnor.tech>
2023-11-13 10:42:51 -0600

    works

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,8 @@
 import { defineCollection, z } from 'astro:content'
 
-
 const postsCollection = defineCollection({
     type: 'content',
-    schema:  z.object({
+    schema: z.object({
         title: z.string(),
         pubDate: z.coerce.date(),
         description: z.string(),
