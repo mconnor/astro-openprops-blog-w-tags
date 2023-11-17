476575145c7ae558b395a0abf6421b886aaf141e
mike connor <mike@mikeconnor.tech>
2023-11-17 12:21:32 -0600

    delete menu.ts

--- src/scripts/menu.ts
+++
@@ -1,8 +0,0 @@
-const obj = document.querySelector('.hamburger')
-const nav = document.querySelector('.nav-links')
-
-obj &&
-    nav &&
-    obj.addEventListener('click', () => {
-        nav.classList.toggle('expanded')
-    })
