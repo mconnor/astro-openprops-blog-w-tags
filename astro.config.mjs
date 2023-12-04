27a051bc0a638808e2ef48bfeb302026126dbd69
mike connor <mike@mikeconnor.tech>
2023-12-04 17:14:43 -0600

    vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,30 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 // your configuration options here...
 // https://docs.astro.build/en/reference/configuration-reference/
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://www.mikeconnor.tech',
-    image: {
-        domains: ['astro.build'],
-        remotePatterns: [{ protocol: 'https' }],
-    },
-    markdown: {
-        drafts: true,
-    },
-    integrations: [
-        mdx({
-            drafts: true,
-        }),
-    ],
-    vite: {
-        ssr: {
-            noExternal: ['open-props'],
-        },
-    },
-})
+  site: 'https://www.mikeconnor.tech',
+  image: {
+    domains: ['astro.build'],
+    remotePatterns: [{
+      protocol: 'https'
+    }]
+  },
+  markdown: {
+    drafts: true
+  },
+  integrations: [mdx({
+    drafts: true
+  })],
+  vite: {
+    ssr: {
+      noExternal: ['open-props']
+    }
+  },
+  output: "server",
+  adapter: vercel()
+});
