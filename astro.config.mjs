c42d37699cd53e938acd1bf1a555c3ee7f563c30
mike connor <mike@mikeconnor.tech>
2023-12-05 14:42:59 -0600

    igrnore

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   site: 'https://www.mikeconnor.tech',
   image: {
     domains: ['astro.build'],
