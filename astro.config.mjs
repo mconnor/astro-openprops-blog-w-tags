9aab328052ec2c62c955f37f39e7b97bb13b2f76
mike connor <mike@mikeconnor.tech>
2023-12-07 13:37:20 -0600

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,31 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
-  output: 'server',
-  site: 'https://www.mikeconnor.tech',
-  image: {
-    domains: ['astro.build'],
-    remotePatterns: [{
-      protocol: 'https'
-    }]
-  },
-  markdown: {
-    drafts: true
-  },
-  integrations: [mdx({
-    drafts: true
-  })],
-  vite: {
-    ssr: {
-      noExternal: ['open-props']
-    }
-  },
-  adapter: vercel()
-});
+    output: 'server',
+    site: 'https://www.mikeconnor.tech',
+    image: {
+        domains: ['astro.build'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+            },
+        ],
+    },
+    markdown: {
+        drafts: true,
+    },
+    integrations: [
+        mdx({
+            drafts: true,
+        }),
+    ],
+    vite: {
+        ssr: {
+            noExternal: ['open-props'],
+        },
+    },
+    adapter: vercel(),
+})
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,7 @@
-const postcssJitProps = require('postcss-jit-props');
-const OpenProps = require('open-props');
-const autoprefixer = require('autoprefixer');
+const postcssJitProps = require('postcss-jit-props')
+const OpenProps = require('open-props')
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
-  plugins: [postcssJitProps(OpenProps), autoprefixer],
-};
+    plugins: [postcssJitProps(OpenProps), autoprefixer],
+}
