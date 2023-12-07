4a7fdbcd063835b863b494b520c7627f6e2826ab
mike connor <mike@mikeconnor.tech>
2023-12-07 13:21:59 -0600

    autoprefixer

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
 const postcssJitProps = require('postcss-jit-props');
 const OpenProps = require('open-props');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  plugins: [postcssJitProps(OpenProps)],
+  plugins: [postcssJitProps(OpenProps), autoprefixer],
 };
