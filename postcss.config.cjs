468d74b47b2e0c119b5cc2d5a58c527b89a31df1
mike connor <mike@mikeconnor.tech>
2023-12-15 17:59:04 -0600

    layouts

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,14 @@
 const postcssJitProps = require('postcss-jit-props')
-const OpenProps = require('open-props')
+const path = require('path');
+
+
 const autoprefixer = require('autoprefixer')
 
 module.exports = {
-    plugins: [postcssJitProps(OpenProps), autoprefixer],
+    plugins: [
+      postcssJitProps({
+        files: [
+          path.resolve(__dirname, 'node_modules/open-props/open-props.min.css'),
+        ]
+      }), autoprefixer],
 }
