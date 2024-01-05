858b942922536455b5ec273446c19b2fdb7da365
mike connor <mike@mikeconnor.tech>
2024-01-05 14:46:59 -0600

    theme

--- src/scripts/select.ts
+++ src/scripts/select.ts
@@ -1,9 +1,7 @@
 import { navigate } from 'astro:transitions/client'
 
 document.addEventListener('astro:page-load', () => {
-    const navSelect = document.querySelector('[data-navigate-select]')
-    navSelect &&
-        navSelect.addEventListener(
+    document.querySelector('[data-navigate-select]')?.addEventListener(
             'change',
             (e: Event) =>
                 e.target && navigate((e.target as HTMLSelectElement).value),
