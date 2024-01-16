5c6514c08c9709bf17658c90cc553aa0a34a1d45
mike connor <mike@mikeconnor.tech>
2024-01-16 16:43:00 -0600

    engine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 
-import vercel from '@astrojs/vercel/serverless'
+
 
 export default defineConfig({
     output: 'static',
