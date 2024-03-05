4c506a99617eae91f3691e0b561805f6080a8718
mike connor <mike@mikeconnor.tech>
2024-03-05 15:49:33 -0600

    rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,12 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from 'markdown-it'
+const parser = new MarkdownIt()
 
 export const prerender = true
-export async function GET() {
+
+export async function GET(context) {
   const posts = await getCollection('blog')
   return rss({
     title: 'Mike Connor | Blog',
@@ -10,11 +14,12 @@ export async function GET() {
       'My personal blog about Astro, Svelte, and other web development topics.',
     site: context.site,
     items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
       link: `/posts/${post.slug}/`,
+      // Note: this will not process components or JSX expressions in MDX files.
+      content: sanitizeHtml(parser.render(post.body)),
+      ...post.data,
     })),
     customData: `<language>en-us</language>`,
+    stylesheet: '/rss/styles.xsl',
   })
 }
