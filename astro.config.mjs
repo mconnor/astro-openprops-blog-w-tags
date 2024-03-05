113e26d81b8165041367e7b7a4a9e753c6bec582
mike connor <mike@mikeconnor.tech>
2024-03-05 15:49:02 -0600

    astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,6 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sentry from '@sentry/astro'
-import { loadEnv } from 'vite'
-
-const { SECRET_TOKEN } = loadEnv(
-  process.env.SENTRY_AUTH_TOKEN,
-  process.cwd(),
-  '',
-)
 
 // var token = loadEnv(process.env.SENTRY_AUTH_TOKEN, process.cwd(), ''),
 // pnpm does not allow you to import modules that are not directly installed in your project. If you are using pnpm, you will need to install vite to use the loadEnv helper.
@@ -17,7 +10,7 @@ export default defineConfig({
   // your configuration options here...
   // https://docs.astro.build/en/reference/configuration-reference/
   output: 'static',
-  site: 'https://www.mikeconnor.tech',
+  site: 'http://localhost:4321',
   image: {
     domains: ['astro.build', 'picsum.photos'],
   },
@@ -29,13 +22,13 @@ export default defineConfig({
       drafts: true,
     }),
     sentry({
-      dsn: 'https://b620d87b24b1fda6b0064305d7ca46e2@o4506328206802944.ingest.sentry.io/4506770210291712',
+      dsn: import.meta.env.SENTRY_DSN,
       release: '1.0.0',
       environment: import.meta.env.MODE,
       sourceMapsUploadOptions: {
         org: 'mike-connor',
         project: 'astro-openprops-blog-w-tags',
-        authToken: SECRET_TOKEN,
+        authToken: import.meta.env.SENTRY_AUTH_TOKEN,
       },
     }),
   ],
