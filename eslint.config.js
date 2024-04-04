bacde513f091cf5196351c754e774f8c60ce39eb
mike connor <mike@mikeconnor.tech>
2024-04-03 20:05:12 -0500

    eslint.config finally

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,65 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginAstro from 'eslint-plugin-astro'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+import js from '@eslint/js'
+import markdown from 'eslint-plugin-markdown'
+import tsParser from '@typescript-eslint/parser'
 
-import js from '@eslint/js';
-import markdown from 'eslint-plugin-markdown';
+// import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   js.configs.recommended,
   ...eslintPluginAstro.configs['flat/recommended'],
+
   {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+
+        // alert: 'readonly',
+      },
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        // myCustomGlobal: "readonly"
+      },
+      // sourceType: string;
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      // "prettier/prettier"?: undefined;
+    },
+    // processor: string;
+    // plugins?: undefined;
+  },
+
+  {
+    files: ['**/*.astro'],
+
+    // Parse the script in `.astro` as TypeScript by adding the following configuration.
+    // It's the setting you need when using TypeScript.
+
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
+      'no-unused-vars': 'warn',
+      // semi: 'warn',  this rule gets tossed out by eslintConfigPrettier
     },
   },
-  eslintConfigPrettier,
+
   {
     plugins: {
       markdown,
@@ -34,8 +78,9 @@ export default [
     files: ['**/*.md/*.js'],
     rules: {
       // 2. Disable other rules.
-      'no-console': 'off',
-      'import/no-unresolved': 'off',
+      // 'no-console': 'off',
+      // 'import/no-unresolved': 'off',
     },
   },
-];
+  eslintConfigPrettier, // eslint-config-prettier last
+]
