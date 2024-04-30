7767d712bc8f6d4fd569a8461311dd5426e46d88
mike connor <mike@mikeconnor.tech>
2024-04-30 14:42:06 -0500

    link to global css file

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,7 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
+  semi: false,
   singleQuote: true,
   trailingComma: 'all',
   plugins: ['prettier-plugin-astro'],
--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
-// ver 1.0.0
-import astro from 'eslint-plugin-astro';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import globals from 'globals';
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-// import lit from 'eslint-plugin-lit';
+// ver 2.0.0
+import eslintPluginAstro from 'eslint-plugin-astro'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+import js from '@eslint/js'
+import markdown from 'eslint-plugin-markdown'
+import tseslint from 'typescript-eslint'
 
-import markdown from 'eslint-plugin-markdown';
+// import tsPlugin from '@typescript-eslint/eslint-plugin';
 
-// const litRules = lit.configs.recommended.rules;
-// const keys = Object.keys(litRules);
-
-export default tseslint.config(
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs['flat/recommended'],
   {
     ignores: [
       'pnpm-lock.yaml',
@@ -20,14 +20,6 @@ export default tseslint.config(
       '**/test.ts',
       'my-custom-cache-directory',
       'src/env.d.ts',
-    ],
-  },
-  {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...astro.configs['flat/recommended'],
-      // ...astro.configs['flat/jsx-a11y-recommended'],
     ],
   },
 
@@ -40,37 +32,22 @@ export default tseslint.config(
       },
       parserOptions: {
         project: true,
-        tsconfigRootDir: import.meta.dirname,
       },
     },
     rules: {
+      // add more rules here
+      // 'no-console': 'off',
       'astro/no-unused-css-selector': 'warn',
-      'astro/missing-client-only-directive-value': 'warn',
       '@typescript-eslint/no-duplicate-type-constituents': 'off',
+      'no-constructor-return': ['error'],
+      'no-sequences': ['error', { allowInParentheses: false }],
     },
   },
-
   {
     // disable type-aware linting on JS files
-    files: ['**/*.js', '**/*.mjs'],
+    files: ['**/*.js'],
     ...tseslint.configs.disableTypeChecked,
   },
-  // {
-  //   files: ['src/web-components/*.ts'],
-
-  //   plugins: {
-  //     lit,
-  //     rules: {
-  //       // ...
-  //       'lit/attribute-value-entities': 'error',
-  //       'lit/binding-positions': 'error',
-  //       'lit/no-duplicate-template-bindings': 'error',
-  //       'lit/no-invalid-html': 'error',
-  //       'lit/no-legacy-template-syntax': 'error',
-  //       'lit/no-property-change-update': 'error',
-  //     },
-  //   },
-  // },
 
   {
     files: ['src/**/*.md'],
@@ -92,6 +69,11 @@ export default tseslint.config(
     },
   },
   {
+    // disable type-aware linting on JS files
+    files: ['**/*.js'],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
     settings: {
       react: {
         version: 'detect',
@@ -99,4 +81,4 @@ export default tseslint.config(
     },
   },
   eslintConfigPrettier, // eslint-config-prettier last
-);
+]
