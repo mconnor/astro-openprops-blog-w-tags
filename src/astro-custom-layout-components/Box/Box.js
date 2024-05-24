f3de181b4dba51b9ca8d6551b56712c667c868f3
mike connor <mike@mikeconnor.tech>
2024-05-24 17:08:28 -0500

    card

--- src/astro-custom-layout-components/Box/Box.js
+++ src/astro-custom-layout-components/Box/Box.js
@@ -39,7 +39,7 @@ export default class Box extends HTMLElement {
   }
 
   set padding(val) {
-    return this.setAttribute('padding', val);
+    this.setAttribute('padding', val);
   }
 
   get borderWidth() {
@@ -47,7 +47,7 @@ export default class Box extends HTMLElement {
   }
 
   set borderWidth(val) {
-    return this.setAttribute('borderWidth', val);
+    this.setAttribute('borderWidth', val);
   }
 
   static get observedAttributes() {
@@ -59,7 +59,7 @@ export default class Box extends HTMLElement {
   }
 
   set invert(val) {
-    if (!!val) {
+    if (val) {
       this.setAttribute('invert', '');
     } else {
       this.removeAttribute('invert');
