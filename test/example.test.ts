2aab1f8e5ef0596110b2cc68e1214fbb6f47db8a
mike connor <mike@mikeconnor.tech>
2024-06-18 19:03:41 -0500

    clean up

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,7 +1,6 @@
 import { experimental_AstroContainer as AstroContainer } from 'astro/container';
 import { expect, test } from 'vitest';
 import Card from '../src/components/Card.astro';
-import GlobalStyle from '../src/styles/GlobalStyle.astro';
 
 test('Card with slots', async () => {
   const container = await AstroContainer.create();
