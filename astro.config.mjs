7765c6c133679c57d5de3fe304907156feb7ebe7
mike connor <mike@mikeconnor.tech>
2024-07-01 16:30:07 -0500

    loadEnv

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import mdx from '@astrojs/mdx';
 import sentry from '@sentry/astro';
 import lit from '@astrojs/lit';
 import vercel from '@astrojs/vercel/static';
-import * as dotenv from 'dotenv';
-import { process } from 'node';
+// import * as dotenv from 'dotenv';
+// import { loadEnv } from 'vite';
+
 // import vercel from "@astrojs/vercel/serverless";
-dotenv.config();
+
 // import { visualizer } from 'rollup-plugin-visualizer';
 // import myIntegration from './my-toolbar-app/my-integration.ts';
 
@@ -27,12 +28,7 @@ export default defineConfig({
   output: 'static',
   site: 'https://astro-openprops-blog-w-tags-git-staging-mike-connors-projects.vercel.app',
   image: {
-    domains: [
-      'kit.fontawesome.com',
-      'astro.build',
-      'picsum.photos',
-      'https://doodleipsum.com',
-    ],
+    domains: ['astro.build', 'picsum.photos', 'https://doodleipsum.com'],
   },
   markdown: {
     drafts: true,
@@ -47,11 +43,11 @@ export default defineConfig({
     sentry({
       dsn: 'https://b620d87b24b1fda6b0064305d7ca46e2@o4506328206802944.ingest.us.sentry.io/4506770210291712',
       release: '1.0.0',
-      environment: process.env.MODE,
+      environment: import.meta.env.MODE,
       sourceMapsUploadOptions: {
         org: 'mike-connor',
         project: 'astro-openprops-blog-w-tags',
-        authToken: process.env.SENTRY_AUTH_TOKEN,
+        authToken: import.meta.env.SENTRY_AUTH_TOKEN,
       },
     }),
   ],
@@ -60,7 +56,7 @@ export default defineConfig({
   },
   vite: {
     ssr: {
-      noExternal: ['date-fns', 'open-props'],
+      noExternal: ['date-fns', 'open-props', 'kit.fontawesome.com'],
     },
   },
   // cacheDir: './my-custom-cache-directory',
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="astro/astro-jsx" />
+interface ImportMetaEnv {
+  readonly SENTRY_DSN: string;
+  readonly SENTRY_AUTH_TOKEN: string;
+  readonly ESLINT_USE_FLAT_CONFIG: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
