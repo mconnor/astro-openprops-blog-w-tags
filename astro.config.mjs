c88d344591c60b0dafb1f06ce483c0338963a7fe
mike connor <mike@mikeconnor.tech>
2024-07-15 17:06:03 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ import { dev } from 'astro';
 
 // // https://astro.build/config
 
-import preact from '@astrojs/preact';
+// import preact from '@astrojs/preact';
 
 import icon from 'astro-icon';
 
@@ -43,19 +43,17 @@ export default defineConfig({
     mdx({
       drafts: true,
     }),
-    // sentry({
-    //   dsn: 'https://b620d87b24b1fda6b0064305d7ca46e2@o4506328206802944.ingest.us.sentry.io/4506770210291712',
-    //   release: '1.0.0',
-    //   environment: import.meta.env.MODE,
-    //   sourceMapsUploadOptions: {
-    //     org: 'mike-connor',
-    //     project: 'astro-openprops-blog-w-tags',
-    //     authToken: import.meta.env.SENTRY_AUTH_TOKEN,
-    //   },
-    // }),
-    preact({
-      include: ['**/preact/*'],
+    sentry({
+      dsn: 'https://b620d87b24b1fda6b0064305d7ca46e2@o4506328206802944.ingest.us.sentry.io/4506770210291712',
+      release: '1.0.0',
+      environment: import.meta.env.MODE,
+      sourceMapsUploadOptions: {
+        org: 'mike-connor',
+        project: 'astro-openprops-blog-w-tags',
+        authToken: import.meta.env.SENTRY_AUTH_TOKEN,
+      },
     }),
+
     icon(),
   ],
   experimental: {
--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 import { getViteConfig } from 'astro/config';
-import { sentryVitePlugin } from '@sentry/vite-plugin';
+// import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 export default getViteConfig({
   test: {
@@ -9,12 +9,12 @@ export default getViteConfig({
   build: {
     sourcemap: true, // Source map generation must be turned on
   },
-  plugins: [
-    // Put the Sentry vite plugin after all other plugins
-    sentryVitePlugin({
-      authToken: import.meta.env.SENTRY_AUTH_TOKEN,
-      org: 'mike-connor',
-      project: 'astro-openprops-blog-w-tags',
-    }),
-  ],
+  // plugins: [
+  //   // Put the Sentry vite plugin after all other plugins
+  //   sentryVitePlugin({
+  //     authToken: import.meta.env.SENTRY_AUTH_TOKEN,
+  //     org: 'mike-connor',
+  //     project: 'astro-openprops-blog-w-tags',
+  //   }),
+  // ],
 });
