ba9e49a54df4b8ab508ed631b2feaf2063d3c3d8
mike connor <mike@mikeconnor.tech>
2024-07-17 11:39:32 -0500

    .prettierrc

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,14 @@
 /** @type {import("prettier").Config} */
 export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
   singleQuote: true,
   trailingComma: 'all',
   tabWidth: 2,
