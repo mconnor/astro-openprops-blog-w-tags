96345dfa069627d255c81d676a9b8e278f6083f7
mike connor <mike@mikeconnor.tech>
2024-10-14 13:36:31 -0500

    remove stylelint-prettier

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('stylelint').Config} */
 
 import stylelintConfig from 'stylelint-config-html';
-import prettier from 'stylelint-prettier';
 
 export default {
   extends: ['stylelint-config-html', 'stylelint-prettier'],
   stylelintConfig,
-  prettier,
+
   rules: {
     'block-no-empty': true,
     'color-function-notation': ['modern'],
