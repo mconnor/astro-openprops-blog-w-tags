020ea4d4c0288c5705f7db0a46d171a544a69979
mike connor <mike@mikeconnor.tech>
2024-10-20 14:00:52 -0500

    types

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('stylelint').Config} */
-
 import stylelintConfig from 'stylelint-config-html';
+import prettier from 'stylelint-prettier';
 
 export default {
-  extends: ['stylelint-config-html', 'stylelint-prettier'],
-  stylelintConfig,
+  extends: [stylelintConfig, prettier],
 
   rules: {
     'block-no-empty': true,
     'color-function-notation': ['modern'],
+    'custom-property-pattern': [
+      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+      {
+        message: 'Expected custom property name to be kebab-case',
+      },
+    ],
   },
 };
---
+++ types/my.d.ts
@@ -0,0 +1,7 @@
+declare module 'stylelint-config-html';
+declare module 'postcss-pow';
+declare module 'postcss-import';
+declare module 'postcss-jit-props';
+declare module 'postcss-pow';
+declare module 'postcss-import';
+declare module 'stylelint-prettier';
